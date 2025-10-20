963432159accfd3ca39553c444959796536c91da
josh098741 <morongemokaya@gmail.com>
2025-10-20 21:21:22 +0300

    Completed user Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -33,6 +33,10 @@ userSchema.pre("save", async function(next){
     next()
 })
 
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
 const User = mongoose.model("User",userSchema)
 
 export default User

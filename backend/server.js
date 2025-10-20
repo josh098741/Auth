390c8aa6df38561a9a16d766772f6ccc331c57dc
josh098741 <morongemokaya@gmail.com>
2025-10-20 21:56:26 +0300

    Addition of Auth Routes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import connectDB from './lib/db.js'
-
+import authRoutes from './routes/authRoutes.js'
+ 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/auth",authRoutes)
 
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
